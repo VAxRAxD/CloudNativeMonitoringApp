4f652989bcf9cfa1bf277956ea6636e00c1cdd9c
Varad Prabhu <varadp763@gmail.com>
2023-06-06 15:50:24 +0530

    Added network speed & disk usage

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ template=Jinja2Templates(directory="templates")
 def main(request: Request):
     cpu_metric=psutil.cpu_percent()
     mem_metric=psutil.virtual_memory().percent
-    message=None
-    if cpu_metric > 80 or mem_metric > 80:
-        message = "High CPU or Memory Detected, scale up!!!"
-    return template.TemplateResponse("main.html",{"request":request,"message":message,"cpu_metric":cpu_metric,"mem_metric":mem_metric})
+    disk_metric=psutil.disk_usage('/').percent
+    nic_info=psutil.net_if_stats()
+    net_metric=0
+    for entry in nic_info:
+        net_metric=max(net_metric,nic_info[entry][2])
+    return template.TemplateResponse("main.html",{"request":request,"cpu_metric":cpu_metric,"mem_metric":mem_metric,"disk_metric":disk_metric,"net_metric":net_metric/10000})
