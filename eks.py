3f78166433c49d7bdb2bfd808b2acbb882b36fb5
Varad Prabhu <varadp763@gmail.com>
2023-06-30 10:07:03 +0530

    Updated Kubernetes Python Client

--- eks.py
+++ eks.py
@@ -7,16 +7,18 @@ api_client=client.ApiClient()
 
 #Deployment Configure
 container = client.V1Container(
-    name="server-monitoring",
-    image="031677989988.dkr.ecr.us-east-1.amazonaws.com/server_monitoring:latest",
-    ports=[client.V1ContainerPort(container_port=8000)]
+    name="server-monitoring-container",
+    image="vaxraxd/monitoring:latest",
+    ports=[client.V1ContainerPort(container_port=8000   )]
 )
 template = client.V1PodTemplateSpec(
     metadata=client.V1ObjectMeta(labels={"app": "server-monitoring"}),
     spec=client.V1PodSpec(containers=[container]),
 )
 spec = client.V1DeploymentSpec(
-    replicas=1  , template=template, selector={"matchLabels":{"app": "server-monitoring"}})
+    replicas=1,
+    template=template,
+    selector={"matchLabels":{"app": "server-monitoring"}})
 
 deployment = client.V1Deployment(
     api_version="apps/v1",
@@ -32,12 +34,12 @@ api_instance.create_namespaced_deployment(
     body=deployment
 )
 
-#Servive Configure
+#Service Configure
 service = client.V1Service(
     metadata=client.V1ObjectMeta(name="server-monitoring-service"),
     spec=client.V1ServiceSpec(
         selector={"app": "server-monitoring"},
-        ports=[client.V1ServicePort(port=8000)]
+        ports=[client.V1ServicePort(port=8000,target_port=8000)]
     )
 )
 
