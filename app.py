b4b5e7def4958275ba840318cec319a262fe202e
Varad Prabhu <varadp763@gmail.com>
2023-07-01 16:35:43 +0530

    UI modification

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-import psutil, speedtest, subprocess, os
+import psutil, speedtest, subprocess, os, urllib.request
 from flask import Flask, render_template, request, redirect
 from database import *
+from apscheduler.schedulers.background import BackgroundScheduler
 
 UPLOAD_FOLDER='keys/'
 
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 
-# subprocess.run(["./inject.sh 44.202.103.183 ubuntu server.pem"],shell=True)
+NET_METRIC,CPU_METRIC,MEM_METRIC,DISK_METRIC=2,None,None,None
+
+def netUSage():
+    global NET_METRIC
+    NET_METRIC=(speedtest.Speedtest().download())/(10**7)
+
+def deviceStat():
+    global CPU_METRIC, MEM_METRIC, DISK_METRIC
+    CPU_METRIC=psutil.cpu_percent()
+    MEM_METRIC=psutil.virtual_memory().percent
+    DISK_METRIC=psutil.disk_usage('/').percent
+
+scheduler = BackgroundScheduler()
+# network_job = scheduler.add_job(netUSage, 'interval', seconds=8, max_instances=5)
+device_job = scheduler.add_job(deviceStat, 'interval', seconds=1, max_instances=2)
+scheduler.start()
+
+
 
 @app.route("/",methods=['GET'])
 def home():
@@ -25,17 +43,14 @@ def addServer():
     subprocess.run([f"./inject.sh {ipaddr} {name} keys/{filename}"],shell=True) 
     return redirect("/")
 
-
 @app.route("/dashboard/",methods=['GET'])
 def dashboard():
-    ip='44.202.103.183'
-    cpu_metric=psutil.cpu_percent()
-    mem_metric=psutil.virtual_memory().percent
-    disk_metric=psutil.disk_usage('/').percent
-    # speed=speedtest.Speedtest().download()
-    net_metric=50
-    # net_metric=speed/(10**7)
-    return render_template("dashboard.html",ip=ip,cpu_metric=cpu_metric,mem_metric=mem_metric,disk_metric=disk_metric,net_metric=net_metric)
+    ip=urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    return render_template("dashboard.html",ip=ip)
 
-if __name__ == '__main__':  
+@app.route("/metrics/",methods=['GET'])
+def metrics():
+    return [{'cpu_metric':CPU_METRIC,'mem_metric':MEM_METRIC,'disk_metric':DISK_METRIC,'net_metric':NET_METRIC}]
+
+if __name__ == '__main__':
     app.run(debug=True)
